b83c96277e24fba826d5f6a2fc15da04c6e5b86b
GPabloMatias <pablo.gonzalez@comafi.com.ar>
2023-02-17 18:32:48 -0300

    Nuevo commit

--- utils.js
+++ utils.js
@@ -26,15 +26,3 @@ document.querySelector('#calcular2').addEventListener('click', () =>{
             } break;
         }
          document.querySelector('#r2').innerHTML = r});
-    
-//     if(op2 == '+'){
-//         r = n1 + n2;
-//     }else if( op2 == '-'){
-//         r = n1 - n2;
-//     }else if(op2 == '*'){
-//         r = n1 * n2;
-//     }else if(op2 == '/'){
-//         r = n1 / n2;
-//     }
-//     document.querySelector('#r2').innerHTML = r;
-// });
