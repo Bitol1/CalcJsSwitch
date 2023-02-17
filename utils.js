dfe6f68aa6fcc86a8b1fc4e5b1428664aa0ccb79
GPabloMatias <gpablomatias1@gmail.com>
2023-02-17 16:42:10 -0300

    Primer commit calculadora

---
+++ utils.js
@@ -0,0 +1,40 @@
+document.querySelector('#calcular2').addEventListener('click', () =>{
+    const n1 = parseInt(document.querySelector('#n11').value);
+    const n2 = parseInt(document.querySelector('#n22').value);
+    const op2 = document.querySelector('#op').value;
+    let r;
+    switch(op2){
+        case'+':{
+            r= n1 + n2;
+        } break;
+
+        case'-':{  
+            r = n1 - n2;
+        } break;
+        case '*':{
+            r = n1 * n2;
+        } break;
+        case'/':{
+            if (n2 == 0){
+                r = "No se puede dividir por 0";
+            }else{ 
+                r = n1 / n2;
+            }
+        } break; 
+            default:{
+                r = "No se puede realizar la operacion";
+            } break;
+        }
+         document.querySelector('#r2').innerHTML = r});
+    
+//     if(op2 == '+'){
+//         r = n1 + n2;
+//     }else if( op2 == '-'){
+//         r = n1 - n2;
+//     }else if(op2 == '*'){
+//         r = n1 * n2;
+//     }else if(op2 == '/'){
+//         r = n1 / n2;
+//     }
+//     document.querySelector('#r2').innerHTML = r;
+// });
